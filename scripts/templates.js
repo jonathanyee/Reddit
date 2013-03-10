4609c5e6302fb7e38f1ad1f965d3172770251393
Raptrex <raptrex@gmail.com>
2013-03-10 11:23:39 -0700

    added self text template

--- scripts/templates.js
+++ scripts/templates.js
@@ -46,6 +46,31 @@ helpers = helpers || Handlebars.helpers; data = data || {};
     + "\" />\n  </div>\n</li>";
   return buffer;
   });
+templates['posttext'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  buffer += "<li>\n  <div class=\"row\">\n    <a href=\"";
+  if (stack1 = helpers.url) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.url; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" class=\"post-link\">";
+  if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</a>\n  </div>\n  <div class=\"row\">\n    ";
+  if (stack1 = helpers.subreddit) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.subreddit; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\n  </div>\n  <div class=\"row\">\n    ";
+  if (stack1 = helpers.selftext) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.selftext; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\n  </div>\n</li>";
+  return buffer;
+  });
 templates['postyoutube'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
